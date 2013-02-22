5fa66d324dadede15ce78d6ff9a980e5101a8327
Jack Lawson <ajacksified@gmail.com>
2013-02-22 09:52:53 -0800

    fix error with bad username, closes issue 7

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.get('/', function(req, res){
   if(req.query.username){
     https.get('https://github.com/users/' + req.query.username + '/contributions_calendar_data', function(response) {
       response.on('data', function(d){
+        try{
+          JSON.parse(d);
+        }catch(e){
+          d = null;
+        }
+
         res.render('index', { calendarData: d, name: req.query.username });
       });
     });
